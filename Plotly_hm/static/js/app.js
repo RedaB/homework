2d55871f93685eb1a468224b9139a8f42dd05cb8
Reda Boubakri <redaboubakri@Redas-MacBook-Pro.local>
2018-12-19 13:23:02 -0700

    fixing some issue

---
+++ Plotly_hm/static/js/app.js
@@ -0,0 +1,98 @@
+// jshint esversion: 6
+
+
+//Building the MeatData
+function buildMetadata(sample) {
+
+  // @TODO: Complete the following function that builds the metadata panel
+
+  // Using `d3.json` to fetch the metadata for a sample
+    d3.json(`/metadata/${sample}`).then(metaData => {
+      
+      // Using d3 to select the panel with id of `#sample-metadata`
+      let PANEL = d3.select('#sample-metadata');
+      
+      // Using `.html("")` to clear any existing metadata
+      PANEL.html("");
+      
+       // Using `Object.entries` to add each key and value pair to the panel
+      Object.entries(metaData).forEach(([key, value]) => {
+        
+        // Using d3 to append new ags for each key-value in the metadata
+        PANEL.append('h6').text(`${key}: ${value}`);
+      });
+
+      // Building the Gauge Chart
+      buildGauge(data.WFREQ);
+    });
+  
+}
+
+
+//Building Buble Chart
+function buildCharts(sample) {
+
+  // @TODO: Use `d3.json` to fetch the sample data for the plots
+  d3.json(`/samples/${sample}`).then(sampleData => {
+    let sampleValues = sampleData.sample_values;
+    let otuIds = sampleData.otu_ids;
+    let otuLabels = sampleData.otu_labels;
+
+  });
+
+    // @TODO: Build a Bubble Chart using the sample data
+    let trace1 = {
+      x: otuIds,
+      y: sampleValues,
+      mode: 'markers',
+      marker: {
+        color: otuIds,
+        size: sampleValues,
+        colorscale: 'Earth'
+      }
+    };
+
+
+    let data = [trace1];
+    
+    let layout = {
+      title: 'OTU IDs vs Sample Values',
+    };
+
+    Plotly.newPlot('bubble', data, layout);
+
+  }
+
+    // @TODO: Build a Pie Chart
+    // HINT: You will need to use slice() to grab the top 10 sample_values,
+    // otu_ids, and labels (10 each).
+
+
+function init() {
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
+
+  // Use the list of sample names to populate the select options
+  d3.json("/names").then((sampleNames) => {
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+
+    // Use the first sample from the list to build the initial plots
+    const firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+}
+
+function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  buildCharts(newSample);
+  buildMetadata(newSample);
+}
+
+// Initialize the dashboard
+init();
